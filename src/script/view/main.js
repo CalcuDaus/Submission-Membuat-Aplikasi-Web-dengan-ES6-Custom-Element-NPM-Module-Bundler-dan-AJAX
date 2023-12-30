37a3830de01599cb92037b8e41cedc68bc5d2766
daus <dausg878@gmail.com>
2023-12-30 22:27:24 +0700

    Responsibilty

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,39 +1,54 @@
 import Swiper from "swiper/bundle";
-import 'swiper/css/bundle';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import "swiper/css/bundle";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const axios = require("axios");
 const option = {
-    headers: {
-        accept: "application/json",
-        Authorization:
-        process.env.BEARER_TOKEN
-      }
-}
+  headers: {
+    accept: "application/json",
+    Authorization: process.env.BEARER_TOKEN,
+  },
+};
 
 const main = () => {
+  // swiper
+  let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-    axios.get(`${process.env.BASE_URL}/search/movie?query=tes&include_adult=true&language=en-US&page=2`,option)
-    .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+  const imgTrenEl = document.querySelector("#img-poster-tren");
+  const imgPopEl = document.querySelector("#img-poster-pop");
+  setInterval(() => {
+    imgTrenEl.src = document.querySelectorAll(
+      ".swiper-slide-active"
+    )[0].children[0].src;
+    imgPopEl.src = document.querySelectorAll(
+      ".swiper-slide-active"
+    )[1].children[0].src;
+  }, 100);
+  // axios.get(`${process.env.BASE_URL}/search/movie?query=tes&include_adult=true&language=en-US&page=2`,option)
+  // .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   })
 
-      // swiper
-      let swiper = new Swiper(".mySwiper", {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+  // Button Toggle
+  const btnToggle = document.querySelector(".hamburger");
+  const navList = document.querySelector(".nav-list");
+  btnToggle.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
 
-      // AOS
-      AOS.init();
+  // AOS
+  AOS.init();
 };
 
 export default main;
